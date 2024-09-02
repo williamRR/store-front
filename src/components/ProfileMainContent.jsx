14aeadea7b083557d1993bc07aa6d4e3bf2917c8
William Rehel <wrehel@falabella.cl>
2024-09-02 18:15:20 -0400

    Refactor ProfileMainContent component to include gender-neutral greeting

--- src/components/ProfileMainContent.jsx
+++ src/components/ProfileMainContent.jsx
@@ -109,7 +109,7 @@ const ProfileMainContent = () => {
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant='h4' gutterBottom>
-        Bienvenido, {user.email}
+        Bienvenid@ {user.email}
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
