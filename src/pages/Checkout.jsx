227e82d82027dac19bb8eb865ac5ebcea1773bac
William Rehel <wrehel@falabella.cl>
2024-10-31 19:06:56 -0300

    Refactor Checkout page to include shipping type selection and handle contact information changes

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -9,6 +9,11 @@ import {
   ListItemText,
   Divider,
   Autocomplete,
+  RadioGroup,
+  Radio,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
 } from '@mui/material';
 import { useCart } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
@@ -23,6 +28,7 @@ const Checkout = () => {
     phone: '',
     email: '',
   });
+  const [shippingType, setShippingType] = useState('pickup'); // Opciones de envío
 
   const { currentUser } = useAuth();
   const {
@@ -63,10 +69,17 @@ const Checkout = () => {
     setSelectedAddress(value);
   };
 
+  // Manejar cambios en el tipo de envío
+  const handleShippingTypeChange = (event) => {
+    setShippingType(event.target.value);
+  };
+
   // Calcular costo de envío
   useEffect(() => {
-    setShippingCost(totalAmount > 30000 ? 0 : 5000);
-  }, [totalAmount]);
+    setShippingCost(
+      shippingType === 'pickup' || totalAmount > 30000 ? 0 : 5000,
+    );
+  }, [totalAmount, shippingType]);
 
   const handleFlowPayment = async () => {
     try {
@@ -92,11 +105,11 @@ const Checkout = () => {
           currency: 'CLP',
           description: 'Compra en tu tienda',
           email: currentUser.email,
-          shippingAddress: selectedAddress,
+          shippingAddress:
+            shippingType === 'pickup' ? 'Retiro en tienda' : selectedAddress,
           contactInfo,
           details,
           payments: [{ paymentTypeId: 10, amount: totalAmount / 1.19 }],
-          // cart,
         },
       );
 
@@ -108,48 +121,81 @@ const Checkout = () => {
   };
 
   return (
-    console.log(contactInfo),
-    (
-      <Box sx={{ p: 4, maxWidth: 600, margin: 'auto' }}>
-        <Typography variant='h4' gutterBottom>
-          Checkout
+    <Box sx={{ p: 4, maxWidth: 600, margin: 'auto' }}>
+      <Typography variant='h4' gutterBottom color='primary' align='center'>
+        Checkout
+      </Typography>
+
+      {/* Datos del Usuario */}
+      <Box component='form' noValidate autoComplete='off' sx={{ mt: 3 }}>
+        <Typography variant='h6' gutterBottom color='secondary'>
+          Datos del Usuario
         </Typography>
+        <TextField
+          label='Nombre'
+          name='name'
+          fullWidth
+          margin='normal'
+          value={contactInfo.name}
+          onChange={handleContactChange}
+          required
+        />
+        <TextField
+          label='Teléfono'
+          name='phone'
+          fullWidth
+          margin='normal'
+          value={contactInfo.phone}
+          onChange={handleContactChange}
+          required
+        />
+        <TextField
+          label='Correo Electrónico'
+          name='email'
+          fullWidth
+          margin='normal'
+          value={contactInfo.email}
+          onChange={handleContactChange}
+          required
+        />
 
-        {/* Datos del Usuario */}
-        <Box component='form' noValidate autoComplete='off' sx={{ mt: 3 }}>
-          <Typography variant='h6' gutterBottom>
-            Datos del Usuario
-          </Typography>
-          <TextField
-            label='Nombre'
-            name='name'
-            fullWidth
-            margin='normal'
-            value={contactInfo.name}
-            onChange={handleContactChange}
-          />
-          <TextField
-            label='Teléfono'
-            name='phone'
-            fullWidth
-            margin='normal'
-            value={contactInfo.phone}
-            onChange={handleContactChange}
-          />
-          <TextField
-            label='Correo Electrónico'
-            name='email'
-            fullWidth
-            margin='normal'
-            value={contactInfo.email}
-            onChange={handleContactChange}
-          />
+        {/* Opciones de tipo de envío */}
+        <FormControl component='fieldset' sx={{ mt: 2 }}>
+          <FormLabel component='legend' color='primary'>
+            Tipo de Envío
+          </FormLabel>
+          <RadioGroup
+            aria-label='tipo de envío'
+            name='shippingType'
+            value={shippingType}
+            onChange={handleShippingTypeChange}
+          >
+            <FormControlLabel
+              value='pickup'
+              control={<Radio color='primary' />}
+              label='Retiro en tienda'
+            />
+            <FormControlLabel
+              value='delivery'
+              control={<Radio color='primary' />}
+              label='Despacho a domicilio ($5.000), exclusivo SANTIAGO, gratis sobre $30.000'
+            />
+            <FormControlLabel
+              value='mail'
+              control={<Radio color='primary' />}
+              label='Correos de Chile (PRECIO POR PAGAR)'
+            />
+          </RadioGroup>
+        </FormControl>
 
-          {/* Autocomplete para seleccionar dirección */}
+        {/* Autocomplete para seleccionar dirección */}
+        {shippingType === 'delivery' && (
           <Autocomplete
             options={addresses}
             getOptionLabel={(option) =>
-              `${option.street} ${option.number}, ${option.city}, ${option.region}`
+              `${option.street.toUpperCase()} ${
+                option.number
+              }, ${option.city.toUpperCase()}, ${option.region.toUpperCase()}`
             }
             renderInput={(params) => (
               <TextField
@@ -157,71 +203,78 @@ const Checkout = () => {
                 label='Seleccionar dirección'
                 variant='outlined'
                 fullWidth
+                required
               />
             )}
             value={selectedAddress}
             onChange={handleAddressSelect}
             sx={{ margin: '10px 0' }}
           />
-        </Box>
-
-        <Divider sx={{ my: 3 }} />
-
-        {/* Resumen del Carrito */}
-        <Box>
-          <Typography variant='h6' gutterBottom>
-            Resumen del Carrito
-          </Typography>
-          <List>
-            {cart.map((item) => (
-              <ListItem key={item.id} disableGutters>
-                <ListItemText
-                  primary={`${item.name} x${item.quantity}`}
-                  secondary={`$${item.price.toLocaleString()} cada uno`}
-                />
-                <Typography variant='body2'>
-                  ${(item.price * item.quantity).toLocaleString()}
-                </Typography>
-              </ListItem>
-            ))}
-          </List>
-          <Divider sx={{ my: 2 }} />
-
-          {/* Total y Costo de Envío */}
-          <Box display='flex' justifyContent='space-between'>
-            <Typography variant='body1'>Subtotal:</Typography>
-            <Typography variant='body1'>
-              ${totalAmount.toLocaleString()}
-            </Typography>
-          </Box>
-          <Box display='flex' justifyContent='space-between'>
-            <Typography variant='body1'>Costo de Envío:</Typography>
-            <Typography variant='body1'>
-              ${shippingCost > 0 ? shippingCost.toLocaleString() : 'Gratis'}
-            </Typography>
-          </Box>
-          <Divider sx={{ my: 2 }} />
-          <Box display='flex' justifyContent='space-between'>
-            <Typography variant='h6'>Total:</Typography>
-            <Typography variant='h6'>
-              ${(totalAmount + shippingCost).toLocaleString()}
-            </Typography>
-          </Box>
-        </Box>
-
-        {/* Botón de Confirmar Compra */}
-        <Button
-          variant='contained'
-          color='primary'
-          onClick={handleFlowPayment}
-          fullWidth
-          sx={{ mt: 3 }}
-          disabled={!selectedAddress}
-        >
-          Confirmar Compra
-        </Button>
+        )}
       </Box>
-    )
+
+      <Divider sx={{ my: 3 }} />
+
+      {/* Resumen del Carrito */}
+      <Box>
+        <Typography variant='h6' gutterBottom color='secondary'>
+          Resumen del Carrito
+        </Typography>
+        <List>
+          {cart.map((item) => (
+            <ListItem key={item.id} disableGutters>
+              <ListItemText
+                primary={`${item.name} x${item.quantity}`}
+                secondary={`$${item.price.toLocaleString()} cada uno`}
+              />
+              <Typography variant='body2'>
+                ${(item.price * item.quantity).toLocaleString()}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+        <Divider sx={{ my: 2 }} />
+
+        {/* Total y Costo de Envío */}
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='body1'>Subtotal:</Typography>
+          <Typography variant='body1'>
+            ${totalAmount.toLocaleString()}
+          </Typography>
+        </Box>
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='body1'>Costo de Envío:</Typography>
+          <Typography variant='body1'>
+            ${shippingCost > 0 ? shippingCost.toLocaleString() : 'Gratis'}
+          </Typography>
+        </Box>
+        <Divider sx={{ my: 2 }} />
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='h6' color='primary'>
+            Total:
+          </Typography>
+          <Typography variant='h6' color='primary'>
+            ${(totalAmount + shippingCost).toLocaleString()}
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* Botón de Confirmar Compra */}
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={handleFlowPayment}
+        fullWidth
+        sx={{ mt: 3 }}
+        disabled={
+          !contactInfo.name ||
+          !contactInfo.phone ||
+          (!selectedAddress && shippingType === 'delivery')
+        }
+      >
+        Confirmar Compra
+      </Button>
+    </Box>
   );
 };
 
